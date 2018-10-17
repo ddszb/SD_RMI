ff4ac8cfdaf7ad08bb1ce314be66738e2e17caf4
gabrielcs23 <gabrielcs23@hotmail.com>
2018-10-17 19:02:43 -0300

    resolvendo merge

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
--- Servidor.java
+++ Servidor.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
