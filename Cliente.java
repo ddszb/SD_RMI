d65c639fe4b2baacf5ebc8d914c0e203ec5e0d4f
Daniel Brasil <zambranobrasil@gmail.com>
2018-10-18 17:43:50 -0300

    remocao do input do usuario pra leitura/escrita

--- Cliente.java
+++ Cliente.java
@@ -4,48 +4,57 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 import java.io.FileReader;
+import java.util.Random;
 
 public class Cliente {
     private Cliente() {}
     public static void main(String[] args) {
         System.out.println("-------------  Cliente  -----------\n\n");
+        //int idClient = Integer.parseInt(args[1]);               // Definição do id do cliente
+
         String host = (args.length < 1) ? null : args[0];
         try {
-            Registry registry = LocateRegistry.getRegistry(host);
+            Registry registry = LocateRegistry.getRegistry(0);
             ClienteServidor stub = (ClienteServidor) registry.lookup("ClienteServidor");
-            //String resposta = stub.digaAloMundo();
-            //System.out.println("resposta: " + resposta);
-        
-
-            Scanner input = new Scanner(System.in);
-            System.out.println("Digite o nome do arquivo:");
-            String nomeArquivo = input.nextLine();
-            boolean toDo = true;
             
+            Random gerador = new Random(666);                   // Gerador de inteiros para as ações aleatórias           
+            
+            Scanner startEvent = new Scanner(System.in);
+            System.out.println("Digite qualquer coisa para iniciar o cliente ");
+            int startFlag = startEvent.nextInt();
+            boolean toDo = true;
+            int cont = 0;
             while(toDo){
-                System.out.println("\nLeitura(1), Escrita(2) ou Sair(3)?");
-                Scanner newInput = new Scanner(System.in);
-                int op = newInput.nextInt();
+                cont += 1;
+                
+                String arqNum = Integer.toString(gerador.nextInt(3));
+                String arqnome = "arquivo" +arqNum + ".txt";    //
+                //System.out.println("Cliente: " + args[1]);
+                System.out.println("Cliente: 1");
+                int op = gerador.nextInt(2) + 1;
+                Thread.sleep(1000);
                 if(op == 1){
-                    System.out.println("Lido: ");
-                    String text = stub.readFile(nomeArquivo);
-                    System.out.println(text);
+                    System.out.println("Fazendo LEITURA em: "+ arqnome +"\n");
+                    String text = stub.readFile(arqnome);
+                    System.out.println("Texto lido: " + text);
+
                 }
-                else{
-
-                    if (op == 2){
-                        System.out.println("O que escrever?");
-                        String conteudo = input.nextLine();
-
-                        if(stub.writeFile(nomeArquivo,conteudo))
-                            System.out.println("Escrita com sucesso");
-                    
-                    }else if (op == 3)
-                        toDo = false;
+                else if (op == 2){
+                        System.out.println("Fazendo ESCRITA em:" + arqnome + "\n");
+                        //String conteudo = "Cliente " + args[1] + "escreveu aqui.";
+                        String conteudo = "Cliente escreveu aqui na vez " + Integer.toString(cont);
+                        if(stub.writeFile(arqnome, conteudo))
+                            System.out.println("Escrita com sucesso");        
                 }
-
+                System.out.println("---------\n");
+                if (cont == 10){ 
+                    toDo = false;
+                    System.out.println("Tudo finalizado! digite qualquer coisa para sair");
+                    startFlag = startEvent.nextInt();
+                }
+                
+                
             }
-
         }catch (NotBoundException | RemoteException | InterruptedException e ) {
             System.err.println("Capturando a exceção no Cliente: " + e.toString());
             }
